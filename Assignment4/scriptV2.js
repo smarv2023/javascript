d9cd7138a37cd1cbe57838d97c23b6a76ee046eb
Marvin <153880760+smarv2023@users.noreply.github.com>
2024-01-05 11:02:41 -0600

    Modified script

--- Assignment4/scriptV2.js
+++ Assignment4/scriptV2.js
@@ -1,53 +1,49 @@
 // Custom JavaScript object for survey results
-var surveyResult ={
-    email: "",
-    age: "",
-    choice: "",
+function SurveyResult(email, age, choice) {
+    this.email = email;
+    this.age = age;
+    this.choice = choice;
 
     // Function to display results
-    sayResult: function () {
-        var result = document.getElementById('usermsg');
+    this.sayResult = function () {
         var resultText = "Success!\n\nThank you for taking the survey.\n\nHere are your results:\n\n" +
             "Email: " + this.email + "\n" +
             "Age: " + this.age + "\n" +
             "Choice: " + this.choice + "\n";
 
-        
-        result.innerText = resultText;
-        result.className = "text-success";
-    }
-};
+        document.getElementById('usermsg').innerText = resultText;
+    };
+}
 
 // Function to validate the form
 function validateForm() {
     var errormsg = ""; // Variable to collect error messages
 
     // Validate email
-    surveyResult.email = document.getElementById('email').value;
-    if (!surveyResult.email || !isValidEmail(surveyResult.email)) {
+    var email = document.getElementById('email').value;
+    if (!email || !isValidEmail(email)) {
         errormsg += "Please enter a valid email address.\n";
     }
 
     // Validate age
-    surveyResult.age = document.getElementById('age').value;
-    if (!surveyResult.age || isNaN(surveyResult.age) || age <= 0) {
+    var age = document.getElementById('age').value;
+    if (!age || isNaN(age) || age <= 0) {
         errormsg += "Please enter a valid age.\n";
     }
 
     // Validate radio buttons (choice)
-    surveyResult.choice = document.querySelector('input[name="choice"]:checked').value;
-    if (!surveyResult.choice) {
+    var choice = document.querySelector('input[name="choice"]:checked');
+    if (!choice) {
         errormsg += "Please select a choice.\n";
     }
 
     // Display error messages or success
     // If no error messages it will print success
     if (errormsg !== "") {
-        var error = document.getElementById('usermsg');
-        error.innerText = errormsg;
-        error.className = "text-danger";
+        document.getElementById('usermsg').innerText = errormsg;
     } else {
         // Create a new SurveyResult object and display results
+        var surveyResult = new SurveyResult(email, age, choice.value);
         surveyResult.sayResult();
     }
 }
